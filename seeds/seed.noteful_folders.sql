47f60148752ce48033d3165e29a832f59854bfb4
Cesar Perez <Cesar.Perez5272@yahoo.com>
2020-04-10 21:10:57 -0400

    'updated'

--- seeds/seed.noteful_folders.sql
+++ seeds/seed.noteful_folders.sql
@@ -1,5 +1,12 @@
-INSERT INTO true_noteful_folders (id, name)
+INSERT INTO true_noteful_folders (name)
 VALUES
-('b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1', 'Important'),
-('b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1', 'Super'),
-('b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1', 'Spangley');
+('Important'),
+('Super'),
+('Spangley');
+
+
+-- INSERT INTO true_noteful_folders (id, name)
+-- VALUES
+-- ('b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1', 'Important'),
+-- ('b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1', 'Super'),
+-- ('b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1', 'Spangley');
