62a8e460f8880905e0d0fb2c3e5b751fd39438f2
Cesar Perez <Cesar.Perez5272@yahoo.com>
2020-04-14 14:40:00 -0400

    'working on it~' :i

--- src/config.js
+++ src/config.js
@@ -6,5 +6,5 @@
   module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://pawnee_parks_department@localhost/noteful_database',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://pawnee_parks_department@localhost/noteful_database',
   }
